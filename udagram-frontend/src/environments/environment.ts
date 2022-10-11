2e0f475b59fb526e2bd2e74b0413a40a8c70b5a9
Ibrahim Kamara <ibrahamacademic@gmail.com>
2022-10-11 19:45:57 +0000

    refactor app to connect frontend to backend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://afdc9cb6285694dc3952dc8a80aece70-1264137805.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
